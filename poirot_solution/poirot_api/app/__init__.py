d6e567a1487aedc33acfd3e962109f1e90ce8286
Zeren Kavaz <zerenkavaz@gmail.com>
2026-02-06 04:15:25 +0300

    Database setup.

--- poirot_solution/poirot_api/app/__init__.py
+++ poirot_solution/poirot_api/app/__init__.py
@@ -1,25 +1,63 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from config import Config
 
+db = SQLAlchemy()  
+
 def create_app(config_class=Config):
-    # 1. Klasör yollarını hesapla (Matematik kısmı)
-    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # .../poirot_api
-    root_dir = os.path.dirname(base_dir) # .../poirot_solution
+    # --- YOL HESAPLAMA ---
+    base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    root_dir = os.path.dirname(base_dir)
     
-    # Web klasörünün tam yolu
     web_folder = os.path.join(root_dir, 'poirot_web')
     template_folder = os.path.join(web_folder, 'templates')
     static_folder = os.path.join(web_folder, 'static')
 
-    # 2. Flask'ı başlat ve yolları göster
     app = Flask(__name__, 
                 template_folder=template_folder,
                 static_folder=static_folder)
     
     app.config.from_object(config_class)
+ 
+    db.init_app(app) 
+    from app.models.scan_record import ScanRecord
 
-    # 3. Rotaları (Blueprint) içeri al
+    with app.app_context():
+        
+        print("\n" + "="*40)
+        
+        #Nereye Bağlanıyoruz?
+        db_url = app.config['SQLALCHEMY_DATABASE_URI']
+        print(f"Bağlanılan Adres: {db_url}")
+        
+        #Hangi Veritabanı Dosyası?
+        if "/poirot_db" in db_url:
+            print("Hedef Veritabanı DOĞRU: poirot_db")
+        else:
+            print("Hedef Veritabanı YANLIŞ!")
+
+        # Tabloyu Oluşturmayı Dene
+        try:
+            db.create_all()
+            print("Tablo oluşturma komutu gönderildi.")
+            
+            #Tablo gerçekten oluştu mu?
+            inspector = db.inspect(db.engine)
+            tables = inspector.get_table_names()
+            print(f"Veritabanındaki Mevcut Tablolar: {tables}")
+            
+            if 'scan_records' in tables:
+                print("BAŞARILI! 'scan_records' tablosu listede var.")
+            else:
+                print("HATA! Komut çalıştı ama tablo listede yok.")
+                
+        except Exception as e:
+            print(f"BİR HATA OLUŞTU: {e}")
+            
+        print("="*40 + "\n")
+
+    # --- Rotaları Ekleme ---
     from app.api.routes import main
     app.register_blueprint(main)
 
